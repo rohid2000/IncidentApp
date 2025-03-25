90eec0fc876ce177857ca49dcbd228805d11f099
Rohid <rsewpersd@syntess.nl>
2025-03-25 16:29:19 +0100

    Started ApiService setup

---
+++ IncidentApp/Fetcher/Fetchers/ApiService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncidentApp.Fetcher.Fetchers
+{
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7015");
+        }
+    }
+}
--- IncidentApp/MauiProgram.cs
+++ IncidentApp/MauiProgram.cs
@@ -18,6 +18,7 @@ public static class MauiProgram
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
+		builder.Services.AddSingleton<HttpClient>();
 
 		return builder.Build();
 	}
