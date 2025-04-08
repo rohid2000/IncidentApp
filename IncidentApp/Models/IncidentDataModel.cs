f3b73f33ef3c0f2218a1c0a73c564e172bf0091f
Rohid <rsewpersd@syntess.nl>
2025-04-08 14:51:00 +0200

    Case matching with API properties

--- IncidentApp/Models/IncidentDataModel.cs
+++ IncidentApp/Models/IncidentDataModel.cs
@@ -8,10 +8,10 @@ namespace IncidentApp.Models
 {
     public class IncidentDataModel
     {
-        public int id { get; set; }
-        public string description { get; set; }
-        public string status { get; set; }
-        public string priority { get; set; }
-        public int userId { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } = "Gemeld";
+        public string Priority { get; set; }
+        public int UserId { get; set; }
     }
 }
