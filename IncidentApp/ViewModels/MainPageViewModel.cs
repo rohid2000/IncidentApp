cc9e390c6e4d4ea3837045d40c465702387774af
Rohid <rsewpersd@syntess.nl>
2025-06-25 13:46:59 +0200

    Progress

---
+++ IncidentApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using IncidentApp.Models;
+using IncidentApp.Services;
+using System.Windows.Input;
+
+namespace IncidentApp.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private string _description;
+
+        public MainPageViewModel()
+        {
+            AddReportedIncidentCommand = new Command(async()->await AddReportedIncident());
+            NavigateToRegisterPageCommand = new Command(async()->await NavigateToRegisterPage());
+            NavigateToLoginPageCommand = new Command(async()->await NavigateToLoginPage());
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        public Command AddReportedIncidentCommand { get; }
+        public Command NavigateToRegisterPageCommand { get; }
+        public Command NavigateToLoginPageCommand { get; }
+
+        private async Task AddReportedIncident()
+        {
+            try
+            {
+                Location location = await LocationFetcher.GetCurrentLocation();
+
+                var incident = new IncidentDataModel
+                {
+                    Description = Description,
+                    Location = $"{location.Latitude} {location.Longitude}",
+                    UserId = UserStateService.user?.Id ?? null
+                };
+
+                await ApiService.AddIncidentAsync(incident);
+
+                Description = string.Empty;
+
+                ShowMessage();
+            }
+            catch (Exception ex)
+            {
+                ShowAlert();
+            }
+        }
+
+        private async Task NavigateToRegisterPage()
+        {
+            await NavigationService.PushAsync<RegisterPage>();
+        }
+
+        private async Task NavigateToLoginPage()
+        {
+            await NavigationService.PushAsync<LoginPage>();
+        }
+    }   
+}
